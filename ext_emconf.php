8d22bb87972b2b61352b78e7b58005da297871ea
JoH asenau <info@cybercraft.de>
2008-07-16 01:13:52 +0700

    0.2.0 > * plugins for the tagcloud and a tagged items list are final * the tagcloud can be pimped with   a searchbox,   a switch between filtermode and surfmode as well as   a calendar for time frame settings * recognition for additional URL parameters

---
+++ ext_emconf.php
@@ -0,0 +1,39 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "tagpack".
+#
+# Auto generated 24-08-2015 15:47
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Tag Pack',
+	'description' => 'All purpose tagging suite. Use tags for almost any allowed table without having to create new DB fields for each of them. Create multifunctional tag clouds using surf or filter mode together with time based settings, a tag breadcrumb menu and a searchbox with autocompletion.',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '0.2.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'TYPO3_version' => '',
+	'PHP_version' => '',
+	'module' => 'mod1',
+	'state' => 'alpha',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'JoH asenau',
+	'author_email' => 'info@cybercraft.de',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+);
+
+?>
